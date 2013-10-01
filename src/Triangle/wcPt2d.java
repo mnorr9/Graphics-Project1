b22192b81354a438ad11901a8317df5f589d96d0
Rick Wallace <rick.m.wallace@gmail.com>
2013-09-30 21:25:32 -0400

    Re-adding Triangle example

---
+++ src/Triangle/wcPt2d.java
@@ -0,0 +1,17 @@
+package Triangle;
+
+public class wcPt2d {
+    
+    public float x;
+    
+    public float y;
+    
+    /**
+     * Creates a new instance of wcPt2d 
+     */
+    public wcPt2d(float xPos, float yPos) {
+        x = xPos;
+        y = yPos;
+    }
+    
+}
